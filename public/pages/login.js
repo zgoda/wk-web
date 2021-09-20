d4967cdc177eb566241f0b8ea6880f419209967c
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-09-20 13:14:08 +0200

    NCU

--- public/pages/login.js
+++ public/pages/login.js
@@ -1,3 +1,8 @@
 export default function Login() {
-  return <section>Login form</section>;
+  return (
+    <section>
+      <h1>Login form</h1>
+      <p>Here will be our login form.</p>
+    </section>
+  );
 }
