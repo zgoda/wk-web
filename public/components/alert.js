000892d02eb6d18291f7428796019cc1eb320687
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-10-24 21:02:41 +0200

    Use state to keep notification style

--- public/components/alert.js
+++ public/components/alert.js
@@ -1,28 +1,27 @@
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import { useNotifications } from '../utils/notifications';
 
 import style from './alert.module.scss';
 
 function Notification({ notification }) {
+  const [notificationStyle, setNotificationStyle] = useState('');
   const { removeNotification } = useNotifications();
 
   useEffect(() => {
+    switch (notification.style) {
+      case 'danger':
+        setNotificationStyle(style.alertDanger);
+        break;
+      case 'warning':
+        setNotificationStyle(style.alertWarning);
+        break;
+      default:
+        setNotificationStyle(style.alertSuccess);
+        break;
+    }
     const timeout = setTimeout(() => removeNotification(notification.id), 4000);
     return () => clearTimeout(timeout);
   }, [notification, removeNotification]);
-
-  let notificationStyle;
-  switch (notification.style) {
-    case 'danger':
-      notificationStyle = style.alertDanger;
-      break;
-    case 'warning':
-      notificationStyle = style.alertWarning;
-      break;
-    default:
-      notificationStyle = style.alertSuccess;
-      break;
-  }
 
   return (
     <div class={notificationStyle}>
