b619bbf8ffe0418a7d0e0c39eceb5445b66de1a9
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-11-03 12:35:12 +0100

    Conditional proxy

--- wmr.config.mjs
+++ wmr.config.mjs
@@ -1,33 +1,34 @@
 import { defineConfig } from 'wmr';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const proxy = createProxyMiddleware({
-  changeOrigin: true,
-  ws: false,
-  logLevel: 'debug',
-  target: 'http://localhost:5000',
-  pathRewrite: {
-    '/api': '/v1/api',
-    '/auth': '/v1/auth',
-  },
-});
-
-// Full list of options: https://wmr.dev/docs/configuration
 export default defineConfig((options) => {
-  return {
-    /* Your configuration here */
+  /** @type {import('wmr').Options} */
+  const config = {
     alias: {
       react: 'preact/compat',
       'react-dom': 'preact/compat',
     },
-    middleware: [
+  };
+  if (options.mode === 'start') {
+    const proxy = createProxyMiddleware({
+      changeOrigin: true,
+      ws: false,
+      logLevel: 'debug',
+      target: 'http://localhost:5000',
+      pathRewrite: {
+        '/api': '/v1/api',
+        '/auth': '/v1/auth',
+      },
+    });
+    config.middleware = [
       (req, res, next) => {
-        if (req.path.match(/^\/api|auth(\/|$)/) && options.mode !== 'build') {
+        if (req.path.match(/^\/api|auth(\/|$)/)) {
           proxy(req, res, next);
         } else {
           next();
         }
       },
-    ],
-  };
+    ];
+  }
+  return config;
 });
