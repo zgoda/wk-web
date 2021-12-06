85bf59f2e4c70737d41afb11e7cf68ec22471c68
zgoda <jarek.zgoda@gmail.com>
2021-12-06 18:09:10 +0100

    Parametrise error message

--- public/components/errorstate.js
+++ public/components/errorstate.js
@@ -1,9 +1,20 @@
-export function ErrorState() {
+import style from './errorstate.module.css';
+
+/**
+ * @typedef {object} ErrorStateProps
+ * @property {string} [message=null]
+ *
+ * @param {ErrorStateProps} props
+ * @returns {JSX.Element}
+ */
+export function ErrorState({ message = null }) {
+  const errorMessage = message || 'Coś poszło nie tak';
+
   return (
     <section>
-      <hgroup>
+      <hgroup style={style.errorbox}>
         <h1>Błąd</h1>
-        <h2>Coś poszło nie tak</h2>
+        <h2>{errorMessage}</h2>
       </hgroup>
     </section>
   );
